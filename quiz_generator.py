67ff2af6db65468680cd9eb1ba09bea43df6cd52
DOALA0155 <kyleshouki@gmail.com>
2020-08-10 16:18:34 +0900

    add romaji

--- quiz_generator.py
+++ quiz_generator.py
@@ -11,12 +11,12 @@ def get_word_speech_list(title):
     words = element.split("\t")
     if element not in [title, "EOS", ""]:
       word = words[0]
+      english = words[3]
       speech = words[4]
-      word_dictionary = {"word": word, "speech": speech}
+      word_dictionary = {"word": word, "speech": speech, "english": english}
       word_speech_list.append(word_dictionary)
 
   return word_speech_list
-
 def get_quiz_sentence(title):
   word_speech_list = get_word_speech_list(title)
 
@@ -25,7 +25,9 @@ def get_quiz_sentence(title):
   answers = []
 
   for word_dictionary in word_speech_list:
-    if "固有名詞" in word_dictionary["speech"]:
+    romaji = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    print([word in romaji for word in word_dictionary["english"]])
+    if ("固有名詞" in word_dictionary["speech"]) or  (True in [word in romaji for word in word_dictionary["english"]]):
       quiz_sentence += " [問題{}] ".format(proper_noun_number)
       answers.append(word_dictionary["word"])
       proper_noun_number += 1
